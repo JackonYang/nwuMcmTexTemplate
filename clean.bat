0b877855a0ec13a80335847ca86c79eecac3d8c3
jackon_win7 <jiekunyang@gmail.com>
2013-04-20 17:48:40 +0800

    论文格式中明确规定的内容，已经配置完成。关于摘要部分 精调和写作方法，已经整理完成。

---
+++ clean.bat
@@ -0,0 +1,14 @@
+@echo off
+del  *.toc /s
+del  *.out  /s
+del  *.aux  /s
+del  *.log  /s
+del  *.synctex /s
+del  *.dvi /s
+
+del  *.bak  /s
+del  *.bbl  /s
+del  *.blg  /s
+del  document\*.bak  /s 
+del  texcode\*.bak  /s
+
